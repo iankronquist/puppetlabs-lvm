b743471dd3f5b60b1ab613a0da543f17d6ff5190
Lucy Wyman <wyman.lucy@gmail.com>
2015-10-19 13:24:27 -0700

    (MODULES 2706) Add tests for size and extents parameters for logical volume type

--- spec/unit/type/logical_volume_spec.rb
+++ spec/unit/type/logical_volume_spec.rb
@@ -25,6 +25,88 @@ describe Puppet::Type.type(:logical_volume) do
     }.to_not raise_error
   end
 
+  it 'invalid logical initial volume size raises error (char)' do
+    expect {
+      resource = Puppet::Type.type(:logical_volume).new(
+        :name         => 'myvg',
+        :ensure       => :present,
+        :volume_group => 'myvg',
+        :initial_size => 'abcd',
+      )
+    }.to raise_error(Puppet::ResourceError,
+                     'Parameter initial_size failed on Logical_volume[myvg]: abcd is not a valid logical volume size')
+  end
 
+  it 'invalid logical initial volume size raises error (suffix)' do
+    expect {
+      resource = Puppet::Type.type(:logical_volume).new(
+        :name         => 'myvg',
+        :ensure       => :present,
+        :volume_group => 'myvg',
+        :initial_size => '20A',
+      )
+    }.to raise_error(Puppet::ResourceError,
+                     'Parameter initial_size failed on Logical_volume[myvg]: 20A is not a valid logical volume size')
+  end
+
+  it 'valid logical volume initial size does not raise error' do
+    expect {
+      resource = Puppet::Type.type(:logical_volume).new(
+        :name         => 'myvg',
+        :ensure       => :present,
+        :volume_group => 'myvg',
+        :initial_size => '20G',
+      )
+    }.to_not raise_error
+  end
+
+  it 'invalid logical volume size raises error (char)' do
+    expect {
+      resource = Puppet::Type.type(:logical_volume).new(
+        :name         => 'myvg',
+        :ensure       => :present,
+        :volume_group => 'myvg',
+        :size         => 'lucy<3',
+      )
+    }.to raise_error(Puppet::ResourceError,
+                     'Parameter size failed on Logical_volume[myvg]: lucy<3 is not a valid logical volume size')
+  end
+
+  it 'invalid logical volume size raises error (suffix)' do
+    expect {
+      resource = Puppet::Type.type(:logical_volume).new(
+        :name         => 'myvg',
+        :ensure       => :present,
+        :volume_group => 'myvg',
+        :size         => '20Q',
+      )
+    }.to raise_error(Puppet::ResourceError,
+                     'Parameter size failed on Logical_volume[myvg]: 20Q is not a valid logical volume size')
+  end
+
+  it 'invalid logical volume extent raises error' do 
+    expect {
+      resource = Puppet::Type.type(:logical_volume).new(
+        :name         => 'zerocool',
+        :ensure       => :present,
+        :volume_group => 'myvg',
+        :size         => '10M',
+        :extents       => 'acidburn',
+      )
+    }.to raise_error(Puppet::Error,
+                     'Parameter extents failed on Logical_volume[zerocool]: acidburn is not a valid logical volume extent')
+  end
+
+  it 'valid logical volume extent does not raise error' do 
+    expect {
+      resource = Puppet::Type.type(:logical_volume).new(
+        :name         => 'zerocool',
+        :ensure       => :present,
+        :volume_group => 'myvg',
+        :size         => '10M',
+        :extents       => '1%vg',
+      )
+    }.to_not raise_error
+  end
 end
 
